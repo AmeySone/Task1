6dfd1cc92d7e75d7f86cbede04c251766beca470
AmeySone <87115628+AmeySone@users.noreply.github.com>
2021-07-08 12:46:25 +0530

    Add files via upload

---
+++ abc.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  8 12:18:00 2021
+
+@author: amey2
+"""
+
+r = float(input())
+
+a = 3.1415*(r**2)
+
+print('Input the radius of the circle:', r)
+print('The area of the circle is:', a)
---
+++ mycap1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  8 12:29:45 2021
+
+@author: amey2
+"""
+
+file = input('Enter filename:' )
+
+ext = file.split(".")
+
+print ('Extension of the file is :' + ext[-1])
+
